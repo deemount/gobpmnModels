3534f20d4ddbd181c38c99e30bd057af3d899ff7
Salvatore Gonda <salvatore.gonda@gmail.com>
2024-04-04 07:59:41 +0200

    Minor fixes in tests; go-mod-upgrade

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/deemount/gobpmnModels
 
 go 1.22.1
 
-require github.com/deemount/gobpmnTypes v0.1.0-alpha
+require (
+	github.com/deemount/gobpmnTypes v0.1.0-alpha
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- pkg/core/Definitions_test.go
+++ pkg/core/Definitions_test.go
@@ -2,27 +2,27 @@ package core
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
+// TestDefinitionsRepository tests the creation of a new repository
 func TestDefinitionsRepository(t *testing.T) {
-	// create a new repository
-	repo := NewDefinitions()
-	t.Logf("result of repo is %v", repo)
-	// check if the repository is not nil
-	if repo == nil {
-		t.Errorf("expected a repository, got nil")
-	}
-}
-
-func TestDefinitionsRepositoryID(t *testing.T) {
-	// create a new repository
-	repo := NewDefinitions()
-	repo.SetID("definitions", "1234")
-	repo.SetCollaboration()
-	repo.SetProcess(0)
-	t.Logf("result of repo is %v", repo)
-	// check if the repository has the correct id
-	if *repo.GetID() != "Definitions_1234" {
-		t.Errorf("expected a correct id, got something else")
-	}
+	t.Run("NewDefinitions()",
+		func(t *testing.T) {
+			// create a new repository
+			repo := NewDefinitions()
+			t.Logf("result of repo is %v", repo)
+			// check if the repository is not nil
+			if repo == nil {
+				t.Errorf("expected a repository, got nil")
+			}
+		})
+	t.Run("SetID()",
+		func(t *testing.T) {
+			// create a new repository
+			repo := NewDefinitions()
+			repo.SetID("definitions", "1234")
+			assert.Equal(t, "Definitions_1234", *repo.GetID(), "want %v, got %v", "Definitions_1234", *repo.GetID())
+		})
 }
